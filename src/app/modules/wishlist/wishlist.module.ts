3a4b32b2a0f331c183cde018b8596daf7cc0c22f
Zachary Bryant <zachary.bryant@outlook.com>
2018-09-08 15:30:42 -0500

    wishlist module init

---
+++ src/app/modules/wishlist/wishlist.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WishlistRoutingModule } from './wishlist-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    WishlistRoutingModule
+  ],
+  declarations: []
+})
+export class WishlistModule { }
