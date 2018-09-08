2b67d55bbcdb50051cafee120c7cc58a00ce5c8a
Zachary Bryant <zachary.bryant@outlook.com>
2018-09-07 21:09:36 -0500

    Under Construction complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'admiralfeb-net-construction';
+  title = 'Admiralfeb\'s Website';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnderConstructionComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
