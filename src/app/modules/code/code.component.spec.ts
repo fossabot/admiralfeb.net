51a10b797af8ad79602157bab1fcbf652b1ab7a1
Zachary Bryant <zachary.bryant@outlook.com>
2018-09-09 06:28:41 -0500

    spec tests updates

--- src/app/modules/code/code.component.spec.ts
+++ src/app/modules/code/code.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodeComponent } from './code.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CodeComponent', () => {
   let component: CodeComponent;
@@ -8,6 +9,7 @@ describe('CodeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ CodeComponent ]
     })
     .compileComponents();
