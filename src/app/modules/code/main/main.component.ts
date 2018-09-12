62760b2532dc24e164ffec97009518fcad9dcd4f
Zachary Bryant <zachary.bryant@outlook.com>
2018-09-11 21:49:59 -0500

    Code section added

--- src/app/modules/code/main/main.component.ts
+++ src/app/modules/code/main/main.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  codeinProcess = [
+    {
+      title: 'Dining Option Selector',
+      description:
+        'Enter items in and let a RNG (Random Number Generator) pick for you.' +
+        '\n Has the default options of Chipotle, FireHouse Subs, and Noodles and Co.'
+    },
+    {
+      title: 'RuneScape User Info',
+      description: 'Utilizing an API by Jagex, this app will access your RuneScape profile and display info.'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
