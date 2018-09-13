84e67b2cab4bfe18af7e936782c8130a1306b870
Zachary Bryant <zachary.bryant@outlook.com>
2018-09-13 22:15:01 +0000

    Updated deploy-test.ps1

--- build_scripts/deploy-test.ps1
+++ build_scripts/deploy-test.ps1
@@ -10,7 +10,7 @@ cd ..
 npm i -g firebase-tools
 write-host "starting deploy...";
 firebase --version;
-firebase deploy --token $fireBaseToken --project $fireBaseProject --only hosting:test --message "Release: $releaseMessage";
+firebase deploy --token $fireBaseToken --only hosting:test --message "Release: $releaseMessage";
 write-host "deployment completed";
 
 Pop-Location
