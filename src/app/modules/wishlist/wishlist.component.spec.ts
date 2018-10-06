0eb3646a501926e5372bd7ea0b37bed854185640
Zachary Bryant <zachary.bryant@outlook.com>
2018-10-05 19:44:21 -0500

    spec test correction

--- src/app/modules/wishlist/wishlist.component.spec.ts
+++ src/app/modules/wishlist/wishlist.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WishlistComponent } from './wishlist.component';
 import { SharedModule } from '../shared-module/shared-module.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 describe('WishlistComponent', () => {
   let component: WishlistComponent;
@@ -11,7 +12,7 @@ describe('WishlistComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [WishlistComponent],
-      imports: [SharedModule, RouterTestingModule]
+      imports: [SharedModule, RouterTestingModule, MatTooltipModule]
     })
       .compileComponents();
   }));
