8132eb5139eb20ee9faaafb8c72378250f85c809
Zachary Bryant <Zachary@admiralfeb.net>
2018-10-06 16:39:14 -0500

    spec test corrections

--- src/app/modules/wishlist/home/home.component.spec.ts
+++ src/app/modules/wishlist/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SharedModule } from '../../shared-module/shared-module.module';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,9 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [SharedModule],
+      declarations: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
