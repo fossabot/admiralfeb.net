2a55483ee5daa559c53b47782a3841be7d18a87e
Zachary Bryant <zachary.bryant@outlook.com>
2018-10-07 22:52:27 -0500

    wishlist updates

--- src/app/modules/shared-module/material/material.module.ts
+++ src/app/modules/shared-module/material/material.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatExpansionModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatTableModule,
     MatTooltipModule,
   ]
 })
--- src/app/modules/wishlist/clothing/clothing.component.ts
+++ src/app/modules/wishlist/clothing/clothing.component.ts
@@ -1,4 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+
+export interface ZEarrings {
+  stone: string;
+  month: string;
+  count: number;
+}
+
+const zEarrings: ZEarrings[] = [
+  {
+    stone: 'Garnett',
+    month: '01-January',
+    count: 1
+  },
+  {
+    stone: 'Amethyst',
+    month: '02-February',
+    count: 1
+  },
+  {
+    stone: 'Aquamarine',
+    month: '03-March',
+    count: 1
+  },
+  {
+    stone: 'Diamond',
+    month: '04-April',
+    count: null
+  },
+  {
+    stone: 'Emerald',
+    month: '05-May',
+    count: 1
+  },
+  {
+    stone: 'Alexandrite',
+    month: '06-June',
+    count: null
+  },
+  {
+    stone: 'Ruby',
+    month: '07-July',
+    count: null
+  },
+  {
+    stone: 'Peridot',
+    month: '08-August',
+    count: null
+  },
+  {
+    stone: 'Sapphire',
+    month: '09-September',
+    count: null
+  },
+  {
+    stone: 'Tourmaline',
+    month: '10-October',
+    count: null
+  },
+  {
+    stone: 'Topaz',
+    month: '11-November',
+    count: null
+  },
+  {
+    stone: 'Zircon',
+    month: '12-December',
+    count: null
+  },
+  {
+    stone: 'Tanzanite',
+    month: '',
+    count: 1
+  },
+];
 
 @Component({
   selector: 'app-clothing',
@@ -6,10 +81,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clothing.component.css']
 })
 export class ClothingComponent implements OnInit {
+  displayedColumns: string[] = ['stone', 'month', 'count'];
+  dataSource = new MatTableDataSource(zEarrings);
 
+  @ViewChild(MatSort) sort: MatSort;
   constructor() { }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
